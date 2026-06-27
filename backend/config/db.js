7771e4401beebb703ad12046458156b3c1764ddd
siddhigithub <siddhidhainje02@gmail.com>
2026-06-27 10:48:40 +0530

    Added DB port for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ async function connectDatabase() {
   pool = mysql.createPool({
     host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER || 'root',
+     port: process.env.DB_PORT || 3306,
     password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'taskmanager',
     waitForConnections: true, // user wait until the connection is available.
