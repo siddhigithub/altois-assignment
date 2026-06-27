c3006eac856011999f69665c46031fb7ceaf7837
siddhigithub <siddhidhainje02@gmail.com>
2026-06-27 12:23:55 +0530

    Added TiDB SSL configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,21 +5,22 @@ let pool;
 async function connectDatabase() {
   pool = mysql.createPool({
     host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
-     port: process.env.DB_PORT || 3306,
     password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'taskmanager',
-    waitForConnections: true, // user wait until the connection is available.
+    waitForConnections: true,
     connectionLimit: 10,
-    timezone: 'Z'
+    timezone: 'Z',
+    ssl: {
+      rejectUnauthorized: true
+    }
   });
 
   const connection = await pool.getConnection();
-  connection.release(); //this will returns the connection back to the pool after been occupied i.e now available.
-  console.log('Connected to MySQL');
+  connection.release();
+  console.log('Connected to Database');
 }
-
-// conection pool is a collection of reusable db connections
 
 function getPool() {
   if (!pool) {
