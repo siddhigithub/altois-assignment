5fb0c0e233fa5b9fb2ac8783c9bee0ffc6d4e159
siddhigithub <siddhidhainje02@gmail.com>
2026-06-27 12:44:38 +0530

    Added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://altois-assignment.onrender.com/api'
 });
 
 export async function getTasks(status) {
